6c1acfffe61aaa06a86114af4674a012fbeb1d0f
Sean Alvarez <alvie0818@gmail.com>
2025-07-27 16:30:07 -0400

    feat(githook): Add githooks for commit format and tag versioning

---
+++ scripts/install-githooks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp .git-hooks/commit-msg .git/hooks/commit-msg
+cp .git-hooks/pre-push .git/hooks/pre-push
+chmod +x .git/hooks/commit-msg
+chmod +x .git/hooks/pre-push
+echo "Git hooks installed."
